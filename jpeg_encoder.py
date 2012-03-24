a904821028a023195f53f5d493ddafa7a2eb7875
Jack Feng <jackfengji@gmail.com>
2012-03-24 11:01:07 +0800

    refact encoder part 1

--- jpeg_encoder.py
+++ jpeg_encoder.py
@@ -1,88 +1,83 @@
 import Image
-import binascii
 import math
+import operator
+
 from util import PythonF5Random as F5Random
-from util import Permutation, BreakException, EmbedData
-from util import jpeg_natural_order
+from util import Permutation
+from util import BreakException
+from util import EmbedData
+from util import JPEG_NATURAL_ORDER
+from util import create_array
 
 from huffman import Huffman
 from DCT import DCT
 
-class JpegInfo(object):
-    def __init__(self, image, comment):
-        self.precision = 8
-        self.comp_num = 3
-        self.com_id = [1, 2, 3]
-        self.ss = 0
-        self.se = 63
-        self.ah = 0
-        self.al = 0
-        self.hsamp_factor = [2, 1, 1]
-        self.vsamp_factor = [2, 1, 1]
-        self.qtable_number = [0, 1, 1]
-        self.dctable_number = [0, 1, 1]
-        self.actable_number = [0, 1, 1]
+def eight_byte(first, second):
+    return (first << 4) + second
 
-        self.components = [None] * self.comp_num
-        self.comp_width = [0] * self.comp_num
-        self.comp_height = [0] * self.comp_num
-        self.last_column_is_dummy = [False] * 3
-        self.last_row_is_dummy = [False] * 3
-        self.block_width = [0] * self.comp_num
-        self.block_height = [0] * self.comp_num
-        self.imageobj = image
-        self.image_width, self.image_height = image.size
+class JpegInfo(object):
+    precision = 8
+    comp_num = 3
+    com_id = [1, 2, 3]
+    ss = 0
+    se = 63
+    ah = 0
+    al = 0
+    hsamp_factor = [2, 1, 1]
+    vsamp_factor = [2, 1, 1]
+    qtable_number = [0, 1, 1]
+    dctable_number = [0, 1, 1]
+    actable_number = [0, 1, 1]
+
+    def __init__(self, image, comment):
+        self.components   = create_array(None, self.comp_num)
+        self.comp_width   = create_array(0, self.comp_num)
+        self.comp_height  = create_array(0, self.comp_num)
+        self.block_width  = create_array(0, self.comp_num)
+        self.block_height = create_array(0, self.comp_num)
+
         self.comment = comment
+        self.image_width, self.image_height = image.size
+        self.pixels = image.load()
+
         self.get_ycc_array()
 
     def down_sample(self, C, comp):
-        result = [[0] * self.comp_width[comp] for i in range(0, self.comp_height[comp])]
-        inrow = 0
-        incol = 0
-        
-        for outrow in range(0, self.comp_height[comp]):
-            bias = 1
-            for outcol in range(0, self.comp_width[comp]):
-                tmp = C[inrow][incol]; incol += 1
-                tmp += C[inrow][incol]; inrow += 1; incol -= 1
-                tmp += C[inrow][incol]; incol += 1
-                tmp += C[inrow][incol] + bias; inrow -= 1; incol += 1
-                result[outrow][outcol] = tmp / 4.0
-                bias ^= 3
-            inrow += 2
-            incol = 0
+        def cal_result(inrow, incol, bias):
+            return sum([C[inrow][incol], 
+                    C[inrow][incol+1],
+                    C[inrow+1][incol],
+                    C[inrow+1][incol+1],
+                    bias]) / 4.0
 
-        return result
+        return [[cal_result(outrow << 1, outcol << 1, outcol % 2 + 1)
+                for outcol in range(self.comp_width[comp])] 
+                for outrow in range(self.comp_height[comp])]
 
     def get_ycc_array(self):
-        self.max_hsamp_factor = max(self.hsamp_factor)
-        self.max_vsamp_factor = max(self.vsamp_factor)
+        max_hsamp_factor = max(self.hsamp_factor)
+        max_vsamp_factor = max(self.vsamp_factor)
 
-        for i in range(0, self.comp_num):
+        for i in range(self.comp_num):
             self.comp_width[i] = int(math.ceil(self.image_width / 8.0) * 8)
-            self.comp_width[i] = self.comp_width[i] / self.max_hsamp_factor * self.hsamp_factor[i]
-            if self.comp_width[i] != self.image_width / self.max_hsamp_factor * self.hsamp_factor[i]:
-                self.last_column_is_dummy[i] = True
-
+            self.comp_width[i] = self.comp_width[i] / max_hsamp_factor * self.hsamp_factor[i]
             self.block_width[i] = int(math.ceil(self.comp_width[i] / 8.0))
 
             self.comp_height[i] = int(math.ceil(self.image_height / 8.0) * 8)
-            self.comp_height[i] = self.comp_height[i] / self.max_vsamp_factor * self.vsamp_factor[i]
-            if self.comp_height[i] != self.image_height / self.max_vsamp_factor * self.vsamp_factor[i]:
-                self.last_row_is_dummy[i] = True
-
+            self.comp_height[i] = self.comp_height[i] / max_vsamp_factor * self.vsamp_factor[i]
             self.block_height[i] = int(math.ceil(self.comp_height[i] / 8.0))
 
-        values = self.imageobj.load()
-        Y = [[0] * self.comp_width[0] for i in range(0, self.comp_height[0])]
-        Cr = [[0] * self.comp_width[0] for i in range(0, self.comp_height[0])]
-        Cb = [[0] * self.comp_width[0] for i in range(0, self.comp_height[0])]
-        for y in range(0, self.image_height):
-            for x in range(0, self.image_width):
+        values = self.pixels
+        Y  = create_array(0, self.comp_height[0], self.comp_width[0])
+        Cb = create_array(0, self.comp_height[0], self.comp_width[0])
+        Cr = create_array(0, self.comp_height[0], self.comp_width[0])
+
+        for y in range(self.image_height):
+            for x in range(self.image_width):
                 r, g, b = values[x, y]
-                Y[y][x] = 0.299 * r + 0.587 * g + 0.114 * b
-                Cr[y][x] = 128 + 0.5 * r - 0.41869 * g - 0.08131 * b
-                Cb[y][x] = 128 + -0.16874 * r - 0.33126 * g + 0.5 * b
+                Y[y][x]  =  0.299   * r + 0.587   * g + 0.114   * b
+                Cb[y][x] = -0.16874 * r - 0.33126 * g + 0.5     * b + 128
+                Cr[y][x] =  0.5     * r - 0.41869 * g - 0.08131 * b + 128
 
         self.components = [Y, self.down_sample(Cb, 1), self.down_sample(Cr, 2)]
 
@@ -93,8 +88,10 @@ class JpegEncoder(object):
     def __init__(self, image, quality, out, comment):
         self.quality = quality
         self.jpeg_obj = JpegInfo(image, comment)
+
         self.image_width, self.image_height = image.size
         self.out = out
+
         self.dct = DCT(self.quality)
         self.huf = Huffman(*image.size)
 
@@ -106,7 +103,6 @@ class JpegEncoder(object):
         self.write_compressed_data()
         self.write_eoi()
         self.out.flush()
-        #self.out.close()
 
     def get_quality(self):
         return self.quality
@@ -130,26 +126,9 @@ class JpegEncoder(object):
         SOI = [0xff, 0xD8]
         self.write_marker(SOI)
 
-        JFIF = [0] * 18
-        JFIF[0] = 0xff
-        JFIF[1] = 0xe0
-        JFIF[2] = 0x00
-        JFIF[3] = 0x10
-        JFIF[4] = 0x4a
-        JFIF[5] = 0x46
-        JFIF[6] = 0x49
-        JFIF[7] = 0x46
-        JFIF[8] = 0x00
-        JFIF[9] = 0x01
-        JFIF[10] = 0x01
-        JFIF[11] = 0x00
-        JFIF[12] = 0x00
-        JFIF[13] = 0x01
-        JFIF[14] = 0x00
-        JFIF[15] = 0x01
-        JFIF[16] = 0x00
-        JFIF[17] = 0x00
-
+        JFIF = [0xff, 0xe0, 0x00, 0x10, 0x4a, 0x46, 
+                0x49, 0x46, 0x00, 0x01, 0x01, 0x00, 
+                0x00, 0x01, 0x00, 0x01, 0x00, 0x00]
         self.write_array(JFIF)
 
         comment = self.jpeg_obj.get_comment()
@@ -159,141 +138,95 @@ class JpegEncoder(object):
             COM.extend(comment)
             self.write_array(COM)
 
-        DQT = [0] * 134
-        DQT[0] = 0xff
-        DQT[1] = 0xdb
-        DQT[2] = 0x00
-        DQT[3] = 0x84
-        offset = 4
-        for i in range(2):
-            DQT[offset] = i
-            offset += 1
-            tmp_array = self.dct.quantum[i]
-            for j in range(64):
-                DQT[offset] = tmp_array[jpeg_natural_order[j]]
-                offset += 1
+        DQT = [0xff, 0xdb, 0x00, 0x84]
+        for k in range(2):
+            DQT.append(k)
+            DQT.extend([self.dct.quantum[k][JPEG_NATURAL_ORDER[i]] for i in range(64)])
         self.write_array(DQT)
 
-        SOF = [0] * 19
-        SOF[0] = 0xff
-        SOF[1] = 0xc0
-        SOF[2] = 0x00
-        SOF[3] = 17
-        SOF[4] = self.jpeg_obj.precision
-        SOF[5] = self.jpeg_obj.image_height >> 8 & 0xff
-        SOF[6] = self.jpeg_obj.image_height & 0xff
-        SOF[7] = self.jpeg_obj.image_width >> 8 & 0xff
-        SOF[8] = self.jpeg_obj.image_width & 0xff
-        SOF[9] = self.jpeg_obj.comp_num
-        index = 10
-        for i in range(SOF[9]):
-            SOF[index] = self.jpeg_obj.com_id[i]; index += 1
-            SOF[index] = (self.jpeg_obj.hsamp_factor[i] << 4) + self.jpeg_obj.vsamp_factor[i]; index += 1
-            SOF[index] = self.jpeg_obj.qtable_number[i]; index += 1
+        SOF = [0xff, 0xc0, 0x00, 0x11, 
+                self.jpeg_obj.precision, 
+                self.jpeg_obj.image_height >> 8 & 0xff, 
+                self.jpeg_obj.image_height & 0xff, 
+                self.jpeg_obj.image_width >> 8 & 0xff, 
+                self.jpeg_obj.image_width & 0xff, 
+                self.jpeg_obj.comp_num]
+        for i in range(self.jpeg_obj.comp_num):
+            SOF.append(self.jpeg_obj.com_id[i])
+            SOF.append(eight_byte(self.jpeg_obj.hsamp_factor[i], self.jpeg_obj.vsamp_factor[i]))
+            SOF.append(self.jpeg_obj.qtable_number[i])
         self.write_array(SOF)
         
-        length = 2
-        index = 4
-        oldindex = 4
-        DHT1 = [0] * 17
-        DHT4 = [0] * 4
-        DHT4[0] = 0xff
-        DHT4[1] = 0xc4
+        DHT = [0xff, 0xc4, 0, 0]
         for i in range(4):
-            byte_len = 0
-            DHT1[index - oldindex] = self.huf.bits[i][0]; index += 1
-            for j in range(1, 17):
-                tmp = self.huf.bits[i][j]
-                DHT1[index - oldindex] = tmp; index += 1
-                byte_len += tmp
+            DHT.extend(self.huf.BITS[i])
+            DHT.extend(self.huf.VAL[i])
 
-            inter_index = index
-            DHT2 = [0] * byte_len
-            for j in range(byte_len):
-                DHT2[index - inter_index] = self.huf.val[i][j]
-                index += 1
-
-            DHT3 = DHT4[:oldindex]
-            DHT3.extend(DHT1)
-            DHT3.extend(DHT2)
-            DHT4 = DHT3
-            oldindex = index
-
-        DHT4[2] = index - 2 >> 8 & 0xff
-        DHT4[3] = index - 2 & 0xff
-        self.write_array(DHT4)
+        DHT[2] = len(DHT) - 2 >> 8 & 0xff
+        DHT[3] = len(DHT) - 2 & 0xff
+        self.write_array(DHT)
 
         SOS = [0] * 14
-        SOS[0] = 0xff
-        SOS[1] = 0xda
-        SOS[2] = 0x00
-        SOS[3] = 12
-        SOS[4] = self.jpeg_obj.comp_num
-        index = 5
-        for i in range(SOS[4]):
-            SOS[index] = self.jpeg_obj.com_id[i]; index += 1
-            SOS[index] = (self.jpeg_obj.dctable_number[i] << 4) + self.jpeg_obj.actable_number[i]; index += 1
+        SOS = [0xff, 0xda, 0x00, 0x0c, self.jpeg_obj.comp_num]
+        for i in range(self.jpeg_obj.comp_num):
+            SOS.append(self.jpeg_obj.com_id[i])
+            SOS.append(eight_byte(self.jpeg_obj.dctable_number[i], self.jpeg_obj.actable_number[i]))
 
-        SOS[index] = self.jpeg_obj.ss; index += 1
-        SOS[index] = self.jpeg_obj.se; index += 1
-        SOS[index] = (self.jpeg_obj.ah << 4) + self.jpeg_obj.al
+        SOS.append(self.jpeg_obj.ss)
+        SOS.append(self.jpeg_obj.se)
+        SOS.append(eight_byte(self.jpeg_obj.ah, self.jpeg_obj.al))
         self.write_array(SOS)
+
+    def _get_coeff(self):
+        dct_array1 = create_array(0.0, 8, 8)
+        dct_array2 = create_array(0.0, 8, 8)
+        dct_array3 = create_array(0, 64)
+
+        coeff = []
+        for r in range(min(self.jpeg_obj.block_height)):
+            for c in range(min(self.jpeg_obj.block_width)):
+                xpos = c * 8
+                ypos = r * 8
+                for comp in range(self.jpeg_obj.comp_num):
+                    indata = self.jpeg_obj.components[comp]
+                    maxa = self.image_height / 2 * self.jpeg_obj.vsamp_factor[comp] - 1
+                    maxb = self.image_width / 2 * self.jpeg_obj.hsamp_factor[comp] - 1
+
+                    for i in range(self.jpeg_obj.vsamp_factor[comp]):
+                        for j in range(self.jpeg_obj.hsamp_factor[comp]):
+                            ia = ypos * self.jpeg_obj.vsamp_factor[comp] + i * 8
+                            ib = xpos * self.jpeg_obj.hsamp_factor[comp] + j * 8
+
+                            for a in range(8):
+                                for b in range(8):
+                                    dct_array1[a][b] = indata[min(ia+a, maxa)][min(ib+b, maxb)]
+
+                            dct_array2 = self.dct.forward_dct(dct_array1)
+                            dct_array3 = self.dct.quantize_block(dct_array2, 
+                                    self.jpeg_obj.qtable_number[comp])
+                            coeff.extend(dct_array3[:64])
+        return coeff
 
     def write_compressed_data(self):
         tmp = 0
-        dct_array1 = [[0.0] * 8 for i in range(8)]
-        dct_array2 = [[0.0] * 8 for i in range(8)]
-        dct_array3 = [0] * 64
 
-        last_dc_value = [0] * self.jpeg_obj.comp_num
-        zero_array = [0] * 64
+        last_dc_value = create_array(0, self.jpeg_obj.comp_num)
+        zero_array = create_array(0, 64)
         width, height = 0, 0
 
         min_block_width = min(self.jpeg_obj.block_width)
         min_block_height = min(self.jpeg_obj.block_height)
 
-        shuffled_index = 0
-        coeff_count = 0
-        for r in range(min_block_height):
-            for c in range(min_block_width):
-                for comp in range(self.jpeg_obj.comp_num):
-                    for i in range(self.jpeg_obj.vsamp_factor[comp]):
-                        for j in range(self.jpeg_obj.hsamp_factor[comp]):
-                            coeff_count += 64
-
-        coeff = [0] * coeff_count
 
         print 'DCT/quantisation starts'
         print self.image_width, 'x', self.image_height
 
-        for r in range(min_block_height):
-            for c in range(min_block_width):
-                xpos = c * 8
-                ypos = r * 8
-                for comp in range(self.jpeg_obj.comp_num):
-                    width = self.jpeg_obj.block_width[comp]
-                    height = self.jpeg_obj.block_height[comp]
-                    indata = self.jpeg_obj.components[comp]
-
-                    for i in range(self.jpeg_obj.vsamp_factor[comp]):
-                        for j in range(self.jpeg_obj.hsamp_factor[comp]):
-                            xblockoffset = j * 8
-                            yblockoffset = i * 8
-                            for a in range(8):
-                                for b in range(8):
-                                    ia = min(ypos * self.jpeg_obj.vsamp_factor[comp] + yblockoffset + a,
-                                            self.image_height / 2 * self.jpeg_obj.vsamp_factor[comp] - 1)
-                                    ib = min(xpos * self.jpeg_obj.hsamp_factor[comp] + xblockoffset + b,
-                                            self.image_width / 2 * self.jpeg_obj.hsamp_factor[comp] - 1)
-                                    dct_array1[a][b] = indata[ia][ib]
-
-                            dct_array2 = self.dct.forward_dct(dct_array1)
-                            dct_array3 = self.dct.quantize_block(dct_array2, self.jpeg_obj.qtable_number[comp])
-                            coeff[shuffled_index:shuffled_index+64] = dct_array3[:64]
-                            shuffled_index += 64
+        coeff = self._get_coeff()
+        coeff_count = len(coeff)
 
         print 'got %d DCT AC/DC coefficients' % coeff_count
         _changed, _embedded, _examined, _expected, _one, _large, _thrown, _zero = 0, 0, 0, 0, 0, 0, 0, 0
+        shuffled_index = 0
         for i in range(coeff_count):
             if i % 64 == 0:
                 continue
